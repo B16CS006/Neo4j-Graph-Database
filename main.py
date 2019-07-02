656267483bfdfbb9a886abb03aae56895ca58ee2
Chetan Prakash Meena <meena.13@iitj.ac.in>
2019-07-02 19:39:20 +0530

    trying new technique

--- main.py
+++ main.py
@@ -23,9 +23,13 @@ def load_field_collection_items(server_url, username, password, database_dir):
 
 
 if(len(sys.argv) != 2):
-    sys.exit('Invalid Argument: Requred 2 argument but ' +  str(len(sys.argv)) + ' are provided')
+    database_dir = '../database/drupal'
+    # sys.exit('Invalid Argument: Requred 2 argument but ' +  str(len(sys.argv)) + ' are provided')
 else:
     database_dir = sys.argv[1]
+
+if(database_dir[-1] != '/'):
+    database_dir = database_dir + '/'
 
 username = "neo4j"
 password = "a392912030502"
@@ -33,23 +37,23 @@ server_url = 'bolt://localhost:7687'
 
 a = DatabaseHandler(server_url, username, password, dir = database_dir)
 
-while(True):
-    try:
-        print('\n\n')
-        print('1: Create New Database')
-        print('2: Delete Whole Database')
-        print('3: Count Nodes')
-        choice = int(input('Enter choice : '))
-        if(choice < 0):
-            a.close()
-            break
-        elif(choice == 0):
-            load_big_database(server_url, username, password, database_dir)
-        elif(choice == 1):
-            a.load_database()
-        elif(choice == 2):
-            a.delete_whole_database()
-        else:
-            print(a.count_nodes())
-    except Exception as e:
-        print('\n', e, '\n')
+# while(True):
+#     try:
+#         print('\n\n')
+#         print('1: Create New Database')
+#         print('2: Delete Whole Database')
+#         print('3: Count Nodes')
+#         choice = int(input('Enter choice : '))
+#         if(choice < 0):
+#             a.close()
+#             break
+#         elif(choice == 0):
+#             load_big_database(server_url, username, password, database_dir)
+#         elif(choice == 1):
+#             a.load_database()
+#         elif(choice == 2):
+#             a.delete_whole_database()
+#         else:
+#             print(a.count_nodes())
+#     except Exception as e:
+#         print('\n', e, '\n')
